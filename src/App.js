826d0daffb5d0c62608b444e5d11906d1f9f3586
alyng <anoolyng@gmail.com>
2021-04-02 06:39:58 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App({login}) {
   return (
-      <><div><p><h1>Hello {login}! Welcome to React</h1> </p> </div></>
+      <><div><p><h1>Hello {login}! Welcome to React</h1> <ul> Here are the list of things you need to do today:</ul> <li> Stand up code commit pipeline</li><li> Make sure when you make changes to your source code, they are committed to git hub <li> New code changes are automatically copied to AWS S3 bucket and made available to users</li> </li></p> </div></>
   );
 }
 
