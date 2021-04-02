e4b37899fd745840c375691c179fc107c1e63237
alyng <anoolyng@gmail.com>
2021-04-02 17:10:15 -0400

    Added a new line to check that this is going to feature branch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App({login}) {
           <li> Stand up code commit pipeline<input type={"checkbox"} checked/> </li>
           <li> Make sure when you make changes to your source code, they are committed to git hub <input type={"checkbox"} checked/></li>
           <li> New code changes are automatically copied to AWS S3 bucket and made available to users <input type={"checkbox"} checked/></li>
+          <li> Create new Rest API <input type={"checkbox"} /></li>
         </p> </div></>
   );
 }
