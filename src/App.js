aed3ad33901dcb1f3e3e3f3f88e37588f4287cc3
alyng <anoolyng@gmail.com>
2021-04-02 16:54:18 -0400

    added checkboxes for fun

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 
 function App({login}) {
   return (
-      <><div><p><h1>Hello {login}! Welcome to React</h1> <ul> Here are the list of things you need to do today:</ul> <li> Stand up code commit pipeline</li><li> Make sure when you make changes to your source code, they are committed to git hub <li> New code changes are automatically copied to AWS S3 bucket and made available to users</li> </li></p> </div></>
+      <><div><p><h1>Hello {login}! Welcome to React</h1> <ul> Here are the list of things you need to do today:</ul>
+          <li> Stand up code commit pipeline<input type={"checkbox"} checked/> </li>
+          <li> Make sure when you make changes to your source code, they are committed to git hub <input type={"checkbox"} checked/></li>
+          <li> New code changes are automatically copied to AWS S3 bucket and made available to users <input type={"checkbox"} checked/></li>
+        </p> </div></>
   );
 }
 
