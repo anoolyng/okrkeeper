5c5773b30a3dfa88c3937e2edcfabe4854175651
David Lyng <lyngda@gmail.com>
2021-04-02 23:50:43 -0400

    Moved data out of components Added Checkbox component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
+import Checkbox from './Checkbox.js';
 import './App.css';
 
-function App({login}) {
+function App( {user, tasks} ) {
   return (
-      <><div><p><h1>Hello {login}! Welcome to React</h1> <ul> Here are the list of things you need to do today:</ul>
-          <li> Stand up code commit pipeline<input type={"checkbox"} checked/> </li>
-          <li> Make sure when you make changes to your source code, they are committed to git hub <input type={"checkbox"} checked/></li>
-          <li> New code changes are automatically copied to AWS S3 bucket and made available to users <input type={"checkbox"} checked/></li>
-          <li> Create new Rest API <input type={"checkbox"} /></li>
-        </p> </div></>
+      <>
+          <h1>Hello {user}! Welcome to React</h1>
+          <p>Here is the list of things you need to do today:</p>
+          <ul>
+              {tasks.map((entry, i) => (
+                  <li key={i}>
+                      {entry.task}
+                      <Checkbox tasks={tasks} entry={entry} index={i} />
+                  </li>
+              ))}
+          </ul>
+      </>
   );
 }
-
 export default App;
